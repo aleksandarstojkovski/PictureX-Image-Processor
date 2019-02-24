359d93c9e1c1e36aba9b1005170fdf4eb24d8f23
Vinsub77 <vinsub77@gmail.com>
2019-02-24 12:30:34 +0100

    Aggiunto file fxml gestito da scene builder

--- src/main/java/ch/supsi/FXApp.java
+++ src/main/java/ch/supsi/FXApp.java
@@ -2,15 +2,30 @@ package ch.supsi;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class FXApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(new Pane(), 800, 600));
-        stage.show();
+        try{
+
+            FXMLLoader loader = new FXMLLoader();
+            URL fxmlLocation = getClass().getResource("/fxml/guiApp.fxml");
+            loader.setLocation(fxmlLocation);
+            AnchorPane rootLayout = (AnchorPane) loader.load();
+            Scene scene = new Scene(rootLayout, 400, 400);
+            stage.setScene(scene);
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public static void main(String[] args) {
