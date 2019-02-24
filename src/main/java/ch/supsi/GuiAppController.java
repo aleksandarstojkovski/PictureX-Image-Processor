b36c31b09471a81f3e1d801f49d2ce305eba73fe
Vinsub77 <vinsub77@gmail.com>
2019-02-24 18:40:45 +0100

    Creato TreeView, dentro a un BorderPanel, del sistema in uso, troppo lunga la creazione

--- src/main/java/ch/supsi/GuiAppController.java
+++ src/main/java/ch/supsi/GuiAppController.java
@@ -1,4 +1,47 @@
 package ch.supsi;
 
-public class GuiAppController {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class GuiAppController{
+
+    public TreeView<Path> treeItemView;
+    private static final String ROOT_FOLDER = "c:/";
+
+    public void initialize() throws Exception {
+
+        TreeItem<Path> treeItem = new TreeItem<Path>(Paths.get( ROOT_FOLDER));
+        treeItem.setExpanded(false);
+
+        // create tree structure
+        createTree(treeItem);
+        treeItemView.setRoot(treeItem);
+    }
+
+    public static void createTree(TreeItem<Path> rootItem) throws IOException {
+
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(rootItem.getValue())) {
+
+            for (Path path : directoryStream) {
+
+                TreeItem<Path> newItem = new TreeItem<Path>(path);
+                newItem.setExpanded(true);
+
+                rootItem.getChildren().add(newItem);
+
+                if (Files.isDirectory(path)&&Files.isReadable(path)) {
+                    createTree(newItem);
+                }
+            }
+        }
+    }
 }
